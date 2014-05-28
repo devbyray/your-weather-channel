74785d205f0bcd213c950cd1c7dd1f48b087972b
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2014-05-28 16:58:35 +0200

    Changed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,38 @@
 var gulp = require('gulp');  
 var sass = require('gulp-sass');  
 var browserSync = require('browser-sync');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var gutil = require('gulp-util');
+
+gulp.task('clean', function () {  
+  return gulp.src('build', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('vendor', function() {  
+  return gulp.src('js/vendor/*.js')
+    .pipe(concat('js/vendor.js'))
+    .pipe(gulp.dest('build'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build'))
+    .on('error', gutil.log)
+});
+
+gulp.task('css', function() {  
+  return gulp.src('css/*.css')
+    .pipe(concat('css/build.css'))
+    .pipe(gulp.dest('build'))
+    .on('error', gutil.log)
+});
+
+gulp.task('html', function() {  
+  return gulp.src('*.html')
+    .pipe(gulp.dest('build'))
+    .on('error', gutil.log)
+});
 
 
 gulp.task('sass', function () {  
@@ -29,4 +61,7 @@ gulp.task('default', ['sass', 'browser-sync'], function () {
     gulp.watch("*.js", ['bs-reload']);
     gulp.watch("js/*.js", ['bs-reload']);
     gulp.watch("js/*/*.js", ['bs-reload']);
+});
+
+gulp.task('build', ['clean', 'vendor', 'css'], function () {  
 });
