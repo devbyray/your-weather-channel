171760aa81ade1ca41988e0206f5ec4c6ab3b049
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2014-05-30 13:50:30 +0200

    Added comments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready( function(){
+  /**
+   * The form is hidden by default
+   */
   $('.wCast_form').hide();
+
+  /**
+   * If the settings button or form button is clicked than the form and wrapper toggleFade();
+   */
   $('#showSettings, button').click( function() {
     $('.wCast_form').fadeToggle('slow');
     $('.wCast_wrapper').fadeToggle('slow');
