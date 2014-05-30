477e15600506f49aa36d4f79e53f460061ef57b0
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2014-05-30 15:55:16 +0200

    Cleaned the main.js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,12 @@ $(document).ready( function(){
   /**
    * The form is hidden by default
    */
-  $('.wCast_form').hide();
+  $('#overlay').hide();
 
   /**
    * If the settings button or form button is clicked than the form and wrapper toggleFade();
    */
   $('#showSettings, button, #closeForm').click( function() {
-    $('.wCast_form').fadeToggle('slow');
-    $('.wCast_wrapper').fadeToggle('slow');
+    $('#overlay').fadeToggle('slow');
   });
 });
